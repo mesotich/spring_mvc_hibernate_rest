556a05dd1b872a707b58b00ccbc9de98ac0809d7
Dmitry Shishkin <4144442@bk.ru>
2022-05-16 22:10:25 +0300

    @ControllerAdvice - глобальная обработка исключений

--- src/main/java/com/zaurtregulov/spring/rest/controller/MyRESTController.java
+++ src/main/java/com/zaurtregulov/spring/rest/controller/MyRESTController.java
@@ -1,13 +1,13 @@
 package com.zaurtregulov.spring.rest.controller;
 
 import com.zaurtregulov.spring.rest.entity.Employee;
-import com.zaurtregulov.spring.rest.exception_handling.EmployeeIncorrectData;
 import com.zaurtregulov.spring.rest.exception_handling.NoSuchEmployeeException;
 import com.zaurtregulov.spring.rest.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -31,19 +31,5 @@ public class MyRESTController {
             throw new NoSuchEmployeeException("There is no Employee with id = "
                     + id + " in DataBase");
         return employee;
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<EmployeeIncorrectData> handleException(NoSuchEmployeeException exception) {
-        EmployeeIncorrectData data = new EmployeeIncorrectData();
-        data.setData(exception.getMessage());
-        return new ResponseEntity<>(data, HttpStatus.NOT_FOUND);
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<EmployeeIncorrectData> handleException(Exception exception) {
-        EmployeeIncorrectData data = new EmployeeIncorrectData();
-        data.setData(exception.getMessage());
-        return new ResponseEntity<>(data, HttpStatus.BAD_REQUEST);
     }
 }
