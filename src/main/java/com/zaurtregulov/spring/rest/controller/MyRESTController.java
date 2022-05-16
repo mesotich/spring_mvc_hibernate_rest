b463117d1d8129c5931543173b4dff31d3149a97
Dmitry Shishkin <4144442@bk.ru>
2022-05-16 23:38:04 +0300

    Добавление работника, обновление работника, удаление работника

--- src/main/java/com/zaurtregulov/spring/rest/controller/MyRESTController.java
+++ src/main/java/com/zaurtregulov/spring/rest/controller/MyRESTController.java
@@ -4,10 +4,7 @@ import com.zaurtregulov.spring.rest.entity.Employee;
 import com.zaurtregulov.spring.rest.exception_handling.NoSuchEmployeeException;
 import com.zaurtregulov.spring.rest.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -31,5 +28,26 @@ public class MyRESTController {
             throw new NoSuchEmployeeException("There is no Employee with id = "
                     + id + " in DataBase");
         return employee;
+    }
+
+    @PostMapping("/employees")
+    public Employee addNewEmployee(@RequestBody Employee employee) {
+        employeeService.saveEmployee(employee);
+        return employee;
+    }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee) {
+        employeeService.saveEmployee(employee);
+        return employee;
+    }
+
+    @DeleteMapping("employees/{id}")
+    public String deleteEmployee(@PathVariable int id) {
+        Employee employee = employeeService.getEmployee(id);
+        if (employee == null)
+            throw new NoSuchEmployeeException("There is no Employee with ID =" + id + " in Database!");
+        employeeService.deleteEmployee(id);
+        return "Employee with ID = " + id + " was deleted!";
     }
 }
